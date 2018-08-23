4afdda010846e195c20dc086fe4e8dcce3991c93
dinushadck <dinusha@duosoftware.com>
2018-08-23 17:18:48 +0530

    add version 5.1.5

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var restify = require('restify');
 var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
 var config = require('config');
+var mongomodels = require('dvp-mongomodels');
 var jwt = require('restify-jwt');
-var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var secret = require('dvp-common/Authentication/Secret.js');
 var authorization = require('dvp-common/Authentication/Authorization.js');
@@ -20,7 +20,6 @@ var port = config.Host.port || 3000;
 var host = config.Host.vdomain || 'localhost';
 var ardsService =  require('./Workers/Trigger/PickAgent.js');
 var scheduleWorker = require('./Workers/SLA/SLAWorker.js');
-var mongoose = require('mongoose');
 
 
 var server = restify.createServer({
@@ -67,91 +66,6 @@ server.use(jwt({secret: secret.Secret}));
 //mongoose.connect(connectionstring);
 
 //var isJSON = require('is-json');
-
-var util = require('util');
-var mongoip=config.Mongo.ip;
-var mongoport=config.Mongo.port;
-var mongodb=config.Mongo.dbname;
-var mongouser=config.Mongo.user;
-var mongopass = config.Mongo.password;
-var mongoreplicaset= config.Mongo.replicaset;
-
-var connectionstring = '';
-
-console.log(mongoip);
-
-mongoip = mongoip.split(',');
-
-console.log(mongoip);
-
-if(util.isArray(mongoip)){
-    
- if(mongoip.length > 1){    
-        
-    mongoip.forEach(function(item){
-        connectionstring += util.format('%s:%d,',item,mongoport)
-    });
-
-    connectionstring = connectionstring.substring(0, connectionstring.length - 1);
-    connectionstring = util.format('mongodb://%s:%s@%s/%s',mongouser,mongopass,connectionstring,mongodb);
-
-    if(mongoreplicaset){
-        connectionstring = util.format('%s?replicaSet=%s',connectionstring,mongoreplicaset) ;
-    }
-  }else{
-      
-      connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip[0],mongoport,mongodb)
-  }
-    
-}else{
-
-    connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',mongouser,mongopass,mongoip,mongoport,mongodb)
-}
-
-console.log(connectionstring);
-
-mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
-
-
-mongoose.connection.on('error', function (err) {
-    console.error( new Error(err));
-    mongoose.disconnect();
-
-});
-
-mongoose.connection.on('opening', function() {
-    console.log("reconnecting... %d", mongoose.connection.readyState);
-});
-
-
-mongoose.connection.on('disconnected', function() {
-    console.error( new Error('Could not connect to database'));
-    mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
-});
-
-mongoose.connection.once('open', function() {
-    console.log("Connected to db");
-
-});
-
-
-mongoose.connection.on('reconnected', function () {
-    console.log('MongoDB reconnected!');
-});
-
-
-process.on('SIGINT', function() {
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
-});
-
-
-
-
-
-
 
 
 ///////////////////////////////Ticket////////////////////////////////////////////////////////////////////////////////////////////
