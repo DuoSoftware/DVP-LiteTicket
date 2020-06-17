4ac03e10589b894ed30554f10f6f5517059c7008
coderaduo <duoacoder@gmail.com>
2020-06-17 08:45:57 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -150,7 +150,7 @@ module.exports = {
         "fileServiceHost": "fileservice.app.veery.cloud",
         "fileServicePort": 5645,
         "fileServiceVersion":"1.0.0.0",
-        "dynamicPort" : false
+        "dynamicPort" : true
     },
 
     "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
